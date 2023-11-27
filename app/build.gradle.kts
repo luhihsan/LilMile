e5ed681192fd972bb1e6e9785307b074ec44bc90
Ihwan Dwi <ihwandrf@gmail.com>
2023-11-28 03:20:45 +0700

    Fix Tampilan
    Login View, Homepage.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.4.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.github.AnyChart:AnyChart-Android:1.1.5")
     implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
