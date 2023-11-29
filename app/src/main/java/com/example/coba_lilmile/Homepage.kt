f324d9b67f49415d8a7c39ca555b64e30e6dd906
Ihwan Dwi <ihwandrf@gmail.com>
2023-11-29 23:05:54 +0700

    add preferences for transfer var

--- app/src/main/java/com/example/coba_lilmile/Homepage.kt
+++ app/src/main/java/com/example/coba_lilmile/Homepage.kt
@@ -4,19 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.graphics.toColor
+import com.example.coba_lilmile.util.PreferenceHelper
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Homepage : AppCompatActivity() {
 
     private lateinit var pageTB: CardView
     private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var namaUser: TextView
+    private lateinit var preference: PreferenceHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
-        /*bottomNavigation.selectedItemId = R.id.item_1*/
+        namaUser = findViewById(R.id.textGreetingUser)
+        preference = PreferenceHelper(this)
+
+        namaUser.setText(preference.getValues("username"))
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId = R.id.menu_1
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation) .itemActiveIndicatorColor = getColorStateList(R.color.mainBlue)
+
+
+
 
         pageTB = findViewById<CardView>(R.id.btnTB)
 
