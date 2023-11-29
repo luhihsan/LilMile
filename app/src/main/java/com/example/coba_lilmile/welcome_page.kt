00c6e2872f5412761dd1c7199ee9dc3e2f160d0c
wrx_365 <93203737+luhihsan@users.noreply.github.com>
2023-11-29 18:04:50 +0700

    button welcome page

--- app/src/main/java/com/example/coba_lilmile/welcome_page.kt
+++ app/src/main/java/com/example/coba_lilmile/welcome_page.kt
@@ -1,11 +1,39 @@
 package com.example.coba_lilmile
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import java.util.prefs.Preferences
 
 class welcome_page : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var btnPindahRegis: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_page)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        btnPindahRegis = findViewById(R.id.btnPindahRegis)
+
+        btnLogin.setOnClickListener {
+            // Navigate to the login activity
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        btnPindahRegis.setOnClickListener {
+            // Navigate to the registration activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
